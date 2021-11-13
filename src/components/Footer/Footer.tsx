b354d6d512242bb8f3b096a625fa194e2f68ba4b
smaranika05 <smaranikadatta05@gmail.com>
2021-11-13 16:52:41 +0530

    first commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FooterContainer, FooterText } from "./FooterStyle";
+
+const Footer: React.FC = () => {
+  return (
+    <FooterContainer>
+      <FooterText>
+        This is acoincap.io clone. All rights reserved.
+      </FooterText>
+    </FooterContainer>
+  );
+};
+
+export { Footer };
